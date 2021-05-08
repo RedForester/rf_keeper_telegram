eb4c07e0a0996855c1bc964be49a7ee47cd9b331
strangecamelcaselogin <strangecamelcaselogin@gmail.com>
2021-05-09 00:51:53 +0300

    feat: Stronger typing for execute, login_to_rf and create_new_node tasks

--- app/rf_tasks.py
+++ app/rf_tasks.py
@@ -1,26 +1,31 @@
 import asyncio
+from typing import TypeVar, Coroutine, Any
 
 from rf_api_client import RfApiClient
-from rf_api_client.models.nodes_api_models import CreateNodePropertiesDto, CreateNodeDto, PositionType
+from rf_api_client.models.nodes_api_models import CreateNodePropertiesDto, CreateNodeDto, PositionType, NodeDto
+from rf_api_client.models.users_api_models import UserDto
 from rf_api_client.rf_api_client import UserAuth
 
 from app.user_context import UserContext
 
 
-def execute(future):
+T = TypeVar('T')
+
+
+def execute(future: Coroutine[Any, Any, T]) -> T:
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     return loop.run_until_complete(future)
 
 
-async def login_to_rf(username: str, password: str):
+async def login_to_rf(username: str, password: str) -> UserDto:
     async with RfApiClient(
         auth=UserAuth(username=username, password=password)
     ) as rf:
         return await rf.users.get_current()
 
 
-async def create_new_node(ctx: UserContext, title: str):
+async def create_new_node(ctx: UserContext, title: str) -> NodeDto:
     async with RfApiClient(
         auth=UserAuth(username=ctx.username, password=ctx.password)
     ) as rf:
