0278eae5b1013c4dd1ff21a6a7c818ed9f2e9576
Denis Mamin <mamin@nppsatek.ru>
2024-12-13 12:40:59 +0300

    Ready: add rf url to env

--- app/utils/rf_links.py
+++ app/utils/rf_links.py
@@ -1,6 +1,12 @@
+import os
+
 def link_to_node(map_id: str, node_id: str) -> str:
-    return f'https://beta.app.redforester.com/mindmap?mapid={map_id}&nodeid={node_id}'
+    url_from_env = os.getenv('RF_URL')
+    base_url = url_from_env if url_from_env is not None else 'https://beta.app.redforester.com'
+    return f'{base_url}/mindmap?mapid={map_id}&nodeid={node_id}'
 
 
 def link_to_file(file_id: str, file_name: str) -> str:
-    return f'https://beta.app.redforester.com/api/files/{file_id}?filename={file_name}'
+    url_from_env = os.getenv('RF_URL')
+    base_url = url_from_env if url_from_env is not None else 'https://beta.app.redforester.com'
+    return f'{base_url}/api/files/{file_id}?filename={file_name}'
